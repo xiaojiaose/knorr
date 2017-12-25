83ae21c79371b17a4d41b57230cdf4955f4c7b6f
mengdewei <mengdewei0@sina.com>
2017-12-25 14:14:24 +0800

    链接数据库

---
+++ include/common.inc.php
@@ -0,0 +1,28 @@
+<?php
+
+define('IN_HEMA', TRUE);
+define('HEMA_ROOT', substr(dirname(__FILE__), 0, -7));
+define('MAGIC_QUOTES_GPC', get_magic_quotes_gpc());
+date_default_timezone_set('PRC');
+header("Content-type:text/html;charset=utf-8");
+
+require_once HEMA_ROOT . './include/global.func.php';
+
+foreach (array('_COOKIE', '_POST', '_GET') as $_request) {
+	foreach ($$_request as $_key => $_value) {
+		$_key{0} != '_' && $$_key = daddslashes($_value);
+	}
+}
+
+require_once HEMA_ROOT . './include/config.inc.php';
+require_once HEMA_ROOT . './include/db_mysql.class.php';
+
+$datetime = time();
+
+$back_url = 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+
+//连接数据库
+$db = new dbstuff;
+$db->connect($dbhost, $dbuser, $dbpw, $dbname, $pconnect, true, $dbcharset);
+$dbuser = $dbpw = $pconnect = NULL;
+$onlineip = $_SERVER['REMOTE_ADDR'];
---
+++ include/global.func.php
@@ -0,0 +1,98 @@
+<?php
+
+if (!defined('IN_HEMA')) {
+	exit('Access Denied');
+}
+
+function daddslashes($string, $force = 0) {
+	!defined('MAGIC_QUOTES_GPC') && define('MAGIC_QUOTES_GPC', get_magic_quotes_gpc());
+	if (!MAGIC_QUOTES_GPC || $force) {
+		if (is_array($string)) {
+			foreach ($string as $key => $val) {
+				$string[$key] = daddslashes($val, $force);
+			}
+		} else {
+			$string = trim(addslashes($string));
+		}
+	}
+	return $string;
+}
+
+function dhtmlspecialchars($string) {
+	if (is_array($string)) {
+		foreach ($string as $key => $val) {
+			$string[$key] = dhtmlspecialchars($val);
+		}
+	} else {
+		$string = preg_replace('/&amp;((#(\d{3,5}|x[a-fA-F0-9]{4}));)/', '&\\1',
+			str_replace(array('&', '"', '<', '>'), array('&amp;', '&quot;', '&lt;', '&gt;'), $string));
+	}
+	return $string;
+}
+
+function cutstr($string, $length, $dot = ' ...') {
+	global $charset;
+
+	if (strlen($string) <= $length) {
+		return $string;
+	}
+
+	$string = str_replace(array('&amp;', '&quot;', '&lt;', '&gt;'), array('&', '"', '<', '>'), $string);
+
+	$strcut = '';
+	if (strtolower($charset) == 'utf-8') {
+
+		$n = $tn = $noc = 0;
+		while ($n < strlen($string)) {
+
+			$t = ord($string[$n]);
+			if ($t == 9 || $t == 10 || (32 <= $t && $t <= 126)) {
+				$tn = 1;
+				$n++;
+				$noc++;
+			} elseif (194 <= $t && $t <= 223) {
+				$tn = 2;
+				$n += 2;
+				$noc += 2;
+			} elseif (224 <= $t && $t <= 239) {
+				$tn = 3;
+				$n += 3;
+				$noc += 2;
+			} elseif (240 <= $t && $t <= 247) {
+				$tn = 4;
+				$n += 4;
+				$noc += 2;
+			} elseif (248 <= $t && $t <= 251) {
+				$tn = 5;
+				$n += 5;
+				$noc += 2;
+			} elseif ($t == 252 || $t == 253) {
+				$tn = 6;
+				$n += 6;
+				$noc += 2;
+			} else {
+				$n++;
+			}
+
+			if ($noc >= $length) {
+				break;
+			}
+
+		}
+		if ($noc > $length) {
+			$n -= $tn;
+		}
+
+		$strcut = substr($string, 0, $n);
+
+	} else {
+		for ($i = 0; $i < $length; $i++) {
+			$strcut .= ord($string[$i]) > 127 ? $string[$i] . $string[++$i] : $string[$i];
+		}
+	}
+
+	$strcut = str_replace(array('&', '"', '<', '>'), array('&amp;', '&quot;', '&lt;', '&gt;'), $strcut);
+
+	return $strcut . $dot;
+}
+?>
