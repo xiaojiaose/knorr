2036e1e0296619c12f2ba68564b72f2cffcc1b18
mengdewei <mengdewei0@sina.com>
2018-01-01 09:10:15 +0800

    add product

--- templates/product.php
+++ templates/product.php
@@ -18,7 +18,7 @@
 	<h1><a href="/">克诺尔</a></h1>
 	<a href="/" class="logo" rel="nofollow">
 		<img src="/templates/images/icon1.png" onerror="this.onerror=null;this.src='/templates/images/icon1.png'" id="loginSrc">
-	</a>	
+	</a>
 </header>
 
 <div class="brand">
@@ -34,14 +34,14 @@
 	<!-- tab -->
 	<div class="product_box">
 		<div class="product_box1 pr_nob">
-			<img src="/templates/images/product_img.jpg" alt="">
+			<img src="/templates/images/images1.jpg" alt="">
 		</div>
 		<div class="product_box2 pr_nob pr_nol">
 			<div class="product_title">
-				空气供给系统
+				空气供给处理系统
 			</div>
 			<div class="product_txt">
-				享有相同的可靠保修服务的全面支持，也为我们的可持续发展目
+				克诺尔可为商用车提供节能、减排、高效、可靠的空压机等产品，同时为提供干燥清洁的压缩空气提供系统化解决方案。
 			</div>
 			<div class="product_input">
 				<a href="#" class="product_inputbox">浏览详情</a>
@@ -51,17 +51,97 @@
 	<div class="product_box">
 		<div class="product_box2 pr_nob">
 			<div class="product_title">
-				空气供给系统
+				车辆控制系统
 			</div>
 			<div class="product_txt">
-				享有相同的可靠保修服务的全面支持，也为我们的可持续发展目
+				克诺尔目前在中国为客户提供电子制动系统EBS+电子稳定性程序ESP组合方案，将更高安全性的领先制动技术带到用户身边。
 			</div>
 			<div class="product_input">
 				<a href="#" class="product_inputbox">浏览详情</a>
 			</div>
 		</div>
 		<div class="product_box1  pr_nob pr_nol">
-			<img src="/templates/images/product_img.jpg" alt="">
+			<img src="/templates/images/images2.jpg" alt="">
+		</div>
+	</div>
+	<div class="product_box">
+		<div class="product_box1 pr_nob">
+			<img src="/templates/images/images3.jpg" alt="">
+		</div>
+		<div class="product_box2 pr_nob pr_nol">
+			<div class="product_title">
+				轮端系统
+			</div>
+			<div class="product_txt">
+				千里之行，始于足下，克诺尔轮端系统产品拥有顶级性能、快速响应速度和出色制动调节性能，使车辆高速行驶时拥有更安全的制动效果。
+			</div>
+			<div class="product_input">
+				<a href="#" class="product_inputbox">浏览详情</a>
+			</div>
+		</div>
+	</div>
+	<div class="product_box">
+		<div class="product_box2 pr_nob">
+			<div class="product_title">
+				传动系统部件
+			</div>
+			<div class="product_txt">
+				克诺尔专业的气动技术还广泛应用于商用车发动机、变速箱、离合器等传动系统，保障车辆传动系统更加高效、安全、可靠的运行。
+			</div>
+			<div class="product_input">
+				<a href="#" class="product_inputbox">浏览详情</a>
+			</div>
+		</div>
+		<div class="product_box1  pr_nob pr_nol">
+			<img src="/templates/images/images4.jpg" alt="">
+		</div>
+	</div>
+	<div class="product_box">
+		<div class="product_box1 pr_nob">
+			<img src="/templates/images/images5.jpg" alt="">
+		</div>
+		<div class="product_box2 pr_nob pr_nol">
+			<div class="product_title">
+				转向系统
+			</div>
+			<div class="product_txt">
+				tedrive开发了智能液压转向系统iHSA提供安全，舒适和与现代驾驶员辅助系统的接口，让车辆更加智能、安全舒适。
+			</div>
+			<div class="product_input">
+				<a href="#" class="product_inputbox">浏览详情</a>
+			</div>
+		</div>
+	</div>
+	<div class="product_box">
+		<div class="product_box2 pr_nob">
+			<div class="product_title">
+				挂车系统
+			</div>
+			<div class="product_txt">
+				克诺尔为挂车提供最安全、智能的制动、轮端、智能控制等系统解决方案，助力运输更加高效安全。
+			</div>
+			<div class="product_input">
+				<a href="#" class="product_inputbox">浏览详情</a>
+			</div>
+		</div>
+		<div class="product_box1  pr_nob pr_nol">
+			<img src="/templates/images/images6.jpg" alt="">
+		</div>
+	</div>
+	<div class="product_box">
+		<div class="product_box1 ">
+			<img src="/templates/images/images7.jpg" alt="">
+		</div>
+		<div class="product_box2 pr_nol">
+			<div class="product_title">
+				售后服务系统
+			</div>
+			<div class="product_txt">
+				克诺尔除了提供最专业的各种维修包外，还有最专业智能的工具套件和故障诊断系统，完美解决您的后顾之忧。
+			</div>
+			<div class="product_input">
+				<a href="#" class="product_inputbox">浏览详情</a>
+			</div>
 		</div>
 	</div>
 </div>	
